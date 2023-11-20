382ad7f5d9ba32afee4b61936c1ad6d5ebbccf4f
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-4.local>
2023-11-20 21:03:38 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+// Home.js
+import React from "react";
+import Navigation from "./Navigation";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+      <Navigation />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/Navigation.js
@@ -0,0 +1,119 @@
+// Navigation.js
+import "../App.js";
+import "../tailwind.css";
+import React from "react";
+import logoImg from "../assets/HorizontalLogo.svg";
+
+const Navigation = () => {
+  return (
+    <nav className="bg-white fixed w-full top-0 border-b border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="https://flowbite.com/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img src={logoImg} className="h-8" alt="Logo" />
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            style={{
+              backgroundColor: "#073863",
+              borderRadius: "10px",
+              padding: "20px",
+              border: "0.5px solid #4486BD",
+              boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+              color: "#ffffff",
+            }}
+          >
+            {" "}
+            Get Started
+          </button>
+
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+          <ul
+            style={{
+              backgroundColor: "#ffffff",
+              color: "#073863",
+            }}
+            className="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 "
+          >
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                aria-current="page"
+                style={{
+                  color: "#073863",
+                }}
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                style={{
+                  color: "#073863",
+                }}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                style={{
+                  color: "#073863",
+                }}
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                style={{
+                  color: "#073863",
+                }}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
