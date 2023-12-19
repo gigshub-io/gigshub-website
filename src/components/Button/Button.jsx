228f03ab3f5cb2408e0caa960182abf865e9a510
Isadora Caputo <isadora.caputo@gmail.com>
2023-12-19 10:14:15 +0100

    polishes cta button style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,15 +3,7 @@ export const Button = ({ children, onClick }) => {
     <button
       type="button"
       onClick={onClick}
-      className="bg-purple rounded-full px-[30.89px] py-[9.67px] text-xs border-[5px]"
-      style={{
-        // border: "solid #D0D0FF",
-        boxShadow: "0 0 5px rgba(0, 0, 0, 0.5)",
-        color: "#ffffff",
-        gap: "6.45px",
-        width: "Fixed (128.94px)",
-        height: "Hug (37.34px)",
-      }}
+      className="bg-purple rounded-full px-[30.89px] py-[9.67px] text-xs border-[5px] border-solid border-pale-purple shadow-button text-white"
     >
       {children}
     </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,19 @@ module.exports = {
       colors: {
         "off-white": "#F3F3FF",
         purple: "#3C3C8E",
+        "pale-purple": "#D0D0FF",
+        white: "#ffffff",
       },
       padding: {
+        button: "30px 10px",
         "30.89px": "30.89px",
         "9.67px": "9.67px",
       },
       border: {
         "5px": "5px",
+      },
+      boxShadow: {
+        button: "0 0 5px rgba(0, 0, 0, 0.5)",
       },
     },
   },
