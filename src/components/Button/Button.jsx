26dffffb9cc53c70c1cf7fe34237d3dd14f5eee6
Isadora Caputo <isadora.caputo@gmail.com>
2023-12-19 15:08:55 +0100

    finishes first version of footer

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ export const Button = ({ children, onClick }) => {
     <button
       type="button"
       onClick={onClick}
-      className="bg-purple rounded-full px-[30.89px] py-[9.67px] text-xs border-[5px] border-solid border-pale-purple shadow-button text-white"
+      className="bg-purple rounded-full px-[30.89px] py-[9.67px] text-xs border-[5px] border-solid border-pale-purple shadow-button text-white w-40"
     >
       {children}
     </button>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Logo } from "../Logo/Logo";
 import { NavLink } from "../Navigation/components/NavLink";
+import { useState } from "react";
+import { Button } from "../Button/Button";
 
 export const Footer = () => {
+  const [emailSubs, setEmailSubs] = useState("");
+
+  const handleInputChange = (entry) => {
+    const value = entry.target.value;
+    setEmailSubs(value);
+  };
+
   return (
     <div className="flex flex-col items-center gap-16 max-w-screen mx-auto bg-dark-purple ">
-      <div className="w-container py-20">
+      <div className="flex flex-row gap-64 w-container py-20">
         <div className="flex flex-col gap-8">
           <Logo kind="inverted" />
-          <ul className="flex flex-row font-sans font-normal leading-6 rounded-lg gap-7 text-sm">
+          <ul className="flex flex-row font-sans font-normal leading-6 rounded-lg gap-6 text-sm">
             <li>
               <NavLink kind="inverted">Find a job</NavLink>
             </li>
@@ -39,7 +48,21 @@ export const Footer = () => {
             </li>
           </ul>
         </div>
-        <div></div>
+        <div>
+          <div className="flex flex-col gap-4 text-sm">
+            <label className="text-pale-purple">Subscribe</label>
+            <div className="flex flex-row gap-4">
+              <input
+                type="text"
+                value={emailSubs}
+                onChange={handleInputChange}
+                className="rounded-full w-60 h-14"
+                placeholder=" Your email here"
+              ></input>
+              <Button className="w-44">Send</Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
