02538e4d36c1402a6162db91147ccdeff3deb080
Isadora Caputo <isadora.caputo@gmail.com>
2023-12-20 19:38:31 +0100

    finishes first version of social proof

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,50 +2,67 @@
 import React from "react";
 import firstImage from "../assets/first-image.png";
 import secondImage from "../assets/second-image.png";
-import thirdImage from "../assets/thrid-image.png";
+import thirdImage from "../assets/third-image.png";
 import fourthImage from "../assets/fourth-image.png";
+import relumeLogo from "../assets/relume-logo.png";
+import webflowLogo from "../assets/webflow-logo.png";
 import { Button } from "../components/Button/Button";
 
 const Home = () => {
   return (
-    <div className="flex flex-row justify-between w-full bg-off-white px-36 py-16 content-around">
-      <div className="flex flex-col gap-6 w-textContainer mt-16">
-        <div className="font-sans text-7xl font-bold text-dark-purple leading-textHero tracking-wide">
-          Turbocharge your job search with{" "}
-          <span className="text-light-purple">GigsHub</span>
+    <div className="flex flex-col w-full bg-off-white px-36 py-16 gap-36">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-col gap-6 w-textContainer mt-16">
+          <div className="font-sans text-7xl font-bold text-dark-purple leading-textHero tracking-wide">
+            Turbocharge your job search with{" "}
+            <span className="text-light-purple">GigsHub</span>
+          </div>
+          <p className="font-sans text-lg font-normal leading-7 text-dark-purple">
+            We automate applications, tailor CVs, and ensure ideal matches, so
+            you can focus on what truly matters - nailing interviews.
+          </p>
+          <div className="flex pt-4">
+            <Button>Get Hired</Button>
+          </div>
         </div>
-        <p className="font-sans text-lg font-normal leading-7 text-dark-purple">
-          We automate applications, tailor CVs, and ensure ideal matches, so you
-          can focus on what truly matters - nailing interviews.
-        </p>
-        <div className="flex pt-4">
-          <Button>Get Hired</Button>
+        <div className="flex gap-2.5">
+          <div className="flex flex-col gap-2.5">
+            <img
+              src={firstImage}
+              alt="Purple rectangle"
+              className="ml-2 h-imageContainer w-imageContainer"
+            />
+            <img
+              src={thirdImage}
+              alt="Girl laughing with pc"
+              className="ml-2 h-imageContainer w-imageContainer"
+            />
+          </div>
+          <div className="flex flex-col gap-2.5">
+            <img
+              src={secondImage}
+              alt="People laughing with pcs"
+              className="mr-2 h-imageContainer w-imageContainer"
+            />
+            <img
+              src={fourthImage}
+              alt="Purple rectangle"
+              className="mr-2 h-imageContainer w-imageContainer"
+            />
+          </div>
         </div>
       </div>
-      <div className="flex gap-2.5">
-        <div className="flex flex-col gap-2.5">
-          <img
-            src={firstImage}
-            alt="Purple rectangle"
-            className="ml-2 h-imageContainer w-imageContainer"
-          />
-          <img
-            src={thirdImage}
-            alt="Girl laughing with pc"
-            className="ml-2 h-imageContainer w-imageContainer"
-          />
-        </div>
-        <div className="flex flex-col gap-2.5">
-          <img
-            src={secondImage}
-            alt="People laughing with pcs"
-            className="mr-2 h-imageContainer w-imageContainer"
-          />
-          <img
-            src={fourthImage}
-            alt="Purple rectangle"
-            className="mr-2 h-imageContainer w-imageContainer"
-          />
+      <div className="flex flex-col items-center gap-5">
+        <p className="font-sans text-lg font-normal leading-7 text-dark-purple">
+          Trusted by the world's best companies
+        </p>
+        <div className="flex flex-row gap-12">
+          <img src={webflowLogo} alt="People laughing with pcs" className="" />
+          <img src={relumeLogo} alt="People laughing with pcs" className="" />
+          <img src={webflowLogo} alt="People laughing with pcs" className="" />
+          <img src={relumeLogo} alt="People laughing with pcs" className="" />
+          <img src={webflowLogo} alt="People laughing with pcs" className="" />
+          <img src={relumeLogo} alt="People laughing with pcs" className="" />
         </div>
       </div>
     </div>
