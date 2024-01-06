d87411e42698b26f7021807c39e9af4551ad7d5e
Isadora Caputo <isadora.caputo@gmail.com>
2024-01-06 23:10:11 +0100

    implements card opening on cardGallery component

--- src/components/CardGallery/CardGallery.jsx
+++ src/components/CardGallery/CardGallery.jsx
@@ -3,6 +3,38 @@ import { Card } from "./subComponents/Card";
 import { Container } from "../Container";
 
 export const CardGallery = () => {
+  const cardsContent = [
+    {
+      heading: "Smart Job Matching",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis.",
+    },
+    {
+      heading: "Custom CV's",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis.",
+    },
+    {
+      heading: "Job Co-pilot",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis.",
+    },
+    {
+      heading: "Job Auto Apply",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis.",
+    },
+    {
+      heading: "Dashboard",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis.",
+    },
+    {
+      heading: "Community",
+      paragraph:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis.",
+    },
+  ];
   return (
     <Container>
       <div className="flex flex-col items-center gap-10">
@@ -12,36 +44,11 @@ export const CardGallery = () => {
         </p>
 
         <div className="grid grid-cols-3 gap-10">
-          <Card heading="Smart Job Matching">
-            {
-              "Our AI, the cupid of job hunting, sifts through CVs, preferences, and job requirements to ensure you meet your..."
-            }
-          </Card>
-          <Card heading="Custom CV's">
-            {
-              "Ever wanted your CV to have a bit of personality? We make it easy! Our platform lets you create CVs as unique..."
-            }
-          </Card>
-          <Card heading="Job Co-pilot">
-            {
-              "Meet your trusty sidekick, the Job Co-pilot! It's your secret weapon for navigating the hiring landscape. No..."
-            }
-          </Card>
-          <Card heading="Job Auto Apply">
-            {
-              "Ever wished you could clone yourself to fill out job applications? We can't do that, but we can automate the tedious..."
-            }
-          </Card>
-          <Card heading="Dashboard">
-            {
-              "Your mission control for job applications! Dive into analytics, spot which companies are actively hiring..."
-            }
-          </Card>
-          <Card heading="Community">
-            {
-              "Join forces with other job seekers in our thriving hub. Track your applications, receive real-time updates..."
-            }
-          </Card>
+          {cardsContent.map((card, index) => (
+            <Card heading={card.heading} key={index}>
+              {card.paragraph}
+            </Card>
+          ))}
         </div>
       </div>
     </Container>
--- src/components/CardGallery/subComponents/Card.jsx
+++ src/components/CardGallery/subComponents/Card.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import checkIcon from "../../../assets/check-icon.svg";
 
-export const Card = ({ heading, children, link }) => {
+export const Card = ({ heading, children }) => {
+  const ReadMore = ({ children }) => {
+    const text = children;
+    const [isReadMore, setIsReadMore] = useState(true);
+    const toggleReadMore = () => {
+      setIsReadMore(!isReadMore);
+    };
+    return (
+      <div className="flex flex-col gap-4">
+        <p className="text">{isReadMore ? `${text.slice(0, 100)}...` : text}</p>
+        <button
+          className="flex font-sans text-off-white text-base font-bold tracking-wide"
+          onClick={toggleReadMore}
+        >
+          {isReadMore ? "Read more" : " Read less"}
+        </button>
+      </div>
+    );
+  };
+
   return (
-    <div className="w-card flex flex-col gap-3 h-card p-7 bg-dark-blue rounded-2xl">
+    <div className="w-card flex flex-col gap-3 h-max p-7 bg-dark-blue rounded-2xl">
       <div>
         <img src={checkIcon} alt="Check Icon" />
       </div>
       <p className="text-off-white text-lg font-sans font-bold tracking-wide">
         {heading}
       </p>
+
       <div className="font-sans text-off-white text-sm tracking-wide font-light leading-5">
-        {children}
-      </div>
-      <div>
-        <a
-          className="font-sans underline text-off-white text-base font-bold tracking-wide"
-          href={link}
-        >
-          Read more
-        </a>
+        <ReadMore>{children}</ReadMore>
       </div>
     </div>
   );
