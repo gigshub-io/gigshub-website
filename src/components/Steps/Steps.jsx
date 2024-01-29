bc150c1a73a185a34303fc75de7ad95e4f05a583
Isadora Caputo <isadora.caputo@gmail.com>
2024-01-29 19:36:18 +0100

    updates Steps component

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -3,10 +3,11 @@ import step from "../../assets/step.png";
 import { Heading } from "../Heading";
 import { Container } from "../Container";
 
+const titleText = "A better way to job search";
+const descriptionText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.";
+
 export const Steps = () => {
-  const titleText = "A better way to job search";
-  const descriptionText =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.";
   return (
     <Container>
       <div className="flex flex-col items-center gap-32">
@@ -14,13 +15,14 @@ export const Steps = () => {
           title={titleText}
           description={descriptionText}
           alignment="center"
+          className="flex"
         />
         <div className="flex flex-col gap-10">
-          <div className="flex flex-row justify-between gap-24">
+          <div className="flex flex-col lg:flex-row items-center lg:justify-start gap-3 lg:gap-24">
             <div>
               <img src={step} alt="Step 1" />
             </div>
-            <div className="flex flex-col py-40 gap-4 w-stepContainer">
+            <div className="flex flex-col py-10 lg:py-40 gap-6 max-w-80 lg:w-stepContainer items-center">
               <h1 className="font-sans font-bold text-3xl tracking-wide text-dark-purple">
                 Step 1
               </h1>
@@ -29,7 +31,7 @@ export const Steps = () => {
                 <br />
                 Suspendisse varius enim in eros elementum tristique.
               </p>
-              <p className="pt-4 font-sans font-normal text-lg tracking-wide leading-7 text-dark-purple">
+              <p className="lg:pt-4 font-sans font-normal text-lg tracking-wide leading-7 text-dark-purple">
                 Duis cursus, mi quis viverra ornare, eros dolor interdum nulla,
                 ut commodo diam libero vitae erat.
               </p>
