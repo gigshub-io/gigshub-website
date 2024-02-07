59bba50d55055fac0791eb9f21ca82bdce9a81f8
Isadora Caputo <isadora.caputo@gmail.com>
2024-02-07 20:09:56 -0300

    finishes responsive step compononent

--- src/components/Steps/Steps.jsx
+++ src/components/Steps/Steps.jsx
@@ -10,7 +10,7 @@ const descriptionText =
 export const Steps = () => {
   return (
     <Container>
-      <div className="flex flex-col items-center gap-32">
+      <div className="flex flex-col items-center gap-16 lg:gap-32">
         <Heading
           title={titleText}
           description={descriptionText}
@@ -19,60 +19,70 @@ export const Steps = () => {
         />
         <div className="flex flex-col gap-10">
           <div className="flex flex-col lg:flex-row items-center lg:justify-start gap-3 lg:gap-24">
-            <div>
+            <div className="hidden lg:flex">
               <img src={step} alt="Step 1" />
             </div>
-            <div className="flex flex-col py-10 lg:py-40 gap-6 max-w-80 lg:w-stepContainer items-center">
+            <div className="flex flex-col py-10 lg:py-40 gap-6 w-80 lg:w-stepContainer items-start">
               <h1 className="font-sans font-bold text-3xl tracking-wide text-dark-purple">
                 Step 1
               </h1>
-              <p className="font-sans font-bold text-lg tracking-wide leading-7 text-dark-purple">
+              <div className="lg:hidden flex">
+                <img src={step} alt="Step 1" />
+              </div>
+              <p className="font-sans font-bold text-sm lg:text-lg tracking-wide leading-7 text-dark-purple">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                <br />
                 Suspendisse varius enim in eros elementum tristique.
               </p>
-              <p className="lg:pt-4 font-sans font-normal text-lg tracking-wide leading-7 text-dark-purple">
+              <p className="lg:pt-4 font-sans font-normal text-sm lg:text-lg tracking-wide leading-7 text-dark-purple">
                 Duis cursus, mi quis viverra ornare, eros dolor interdum nulla,
                 ut commodo diam libero vitae erat.
               </p>
             </div>
           </div>
-          <div className="flex flex-row justify-between gap-24">
-            <div className="flex flex-col py-40 gap-4 w-stepContainer">
-              <h1 className="font-sans font-bold text-3xl tracking-wide text-dark-purple">
-                Step 2
-              </h1>
-              <p className="font-sans font-bold text-lg tracking-wide leading-7 text-dark-purple">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                <br />
-                Suspendisse varius enim in eros elementum tristique.
-              </p>
-              <p className="pt-4 font-sans font-normal text-lg tracking-wide leading-7 text-dark-purple">
-                Duis cursus, mi quis viverra ornare, eros dolor interdum nulla,
-                ut commodo diam libero vitae erat.
-              </p>
+          <div className="flex flex-col gap-10">
+            <div className="flex flex-col lg:flex-row items-center lg:justify-start gap-3 lg:gap-24">
+              <div className="flex flex-col py-10 lg:py-40 gap-6 w-80 lg:w-stepContainer items-start">
+                <h1 className="font-sans font-bold text-3xl tracking-wide text-dark-purple">
+                  Step 2
+                </h1>
+                <div className="lg:hidden flex">
+                  <img src={step} alt="Step 2" />
+                </div>
+                <p className="font-sans font-bold text-sm lg:text-lg tracking-wide leading-7 text-dark-purple">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Suspendisse varius enim in eros elementum tristique.
+                </p>
+                <p className="lg:pt-4 font-sans font-normal text-sm lg:text-lg tracking-wide leading-7 text-dark-purple">
+                  Duis cursus, mi quis viverra ornare, eros dolor interdum
+                  nulla, ut commodo diam libero vitae erat.
+                </p>
+              </div>
+              <div className="hidden lg:flex">
+                <img src={step} alt="Step 2" />
+              </div>
             </div>
-            <div>
-              <img src={step} alt="Step 2" />
-            </div>
-          </div>
-          <div className="flex flex-row justify-between gap-24">
-            <div>
-              <img src={step} alt="Step 3" />
-            </div>
-            <div className="flex flex-col py-40 gap-4 w-stepContainer">
-              <h1 className="font-sans font-bold text-3xl tracking-wide text-dark-purple">
-                Step 3
-              </h1>
-              <p className="font-sans font-bold text-lg tracking-wide leading-7 text-dark-purple">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                <br />
-                Suspendisse varius enim in eros elementum tristique.
-              </p>
-              <p className="pt-4 font-sans font-normal text-lg tracking-wide leading-7 text-dark-purple">
-                Duis cursus, mi quis viverra ornare, eros dolor interdum nulla,
-                ut commodo diam libero vitae erat.
-              </p>
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-col lg:flex-row items-center lg:justify-start gap-3 lg:gap-24">
+                <div className="hidden lg:flex">
+                  <img src={step} alt="Step 3" />
+                </div>
+                <div className="flex flex-col py-10 lg:py-40 gap-6 w-80 lg:w-stepContainer items-start">
+                  <h1 className="font-sans font-bold text-3xl tracking-wide text-dark-purple">
+                    Step 3
+                  </h1>
+                  <div className="lg:hidden flex">
+                    <img src={step} alt="Step 3" />
+                  </div>
+                  <p className="font-sans font-bold text-sm lg:text-lg tracking-wide leading-7 text-dark-purple">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum tristique.
+                  </p>
+                  <p className="lg:pt-4 font-sans font-normal text-sm lg:text-lg tracking-wide leading-7 text-dark-purple">
+                    Duis cursus, mi quis viverra ornare, eros dolor interdum
+                    nulla, ut commodo diam libero vitae erat.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
