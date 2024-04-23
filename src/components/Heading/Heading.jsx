506df1750ec4d86b4383c45fc132bbfa93874cd2
FouadAE <fouad.aitelhaj@edu.uca.ma>
2024-04-23 18:22:18 +0100

    fix - update Heading

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -18,11 +18,11 @@ export const Heading = ({
         className={`flex flex-col gap-${gap} w-headingMobileContainer lg:w-headingContainer text-${alignment}`}
       >
         <p
-          className={`font-sans text-${textSize} font-bold leading-7 text-dark-purple leading-headingCardsContainer tracking-wide`}
+          className={`font-sans text-${textSize} font-bold leading-7 text-dark-purple leading-headingCardsContainer tracking-wide max-sm:text-center`}
         >
           {title}
         </p>
-        <p className="font-sans text-sm lg:text-lg font-normal leading-7 text-dark-purple tracking-wide">
+        <p className="font-sans text-sm lg:text-lg font-normal leading-7 text-dark-purple tracking-wide max-sm:text-center">
           {description}
         </p>
       </div>
