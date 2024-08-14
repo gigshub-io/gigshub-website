eb346cad55dabd6c881f62418643dcaf755bb4c2
Giulia Cellerino <giulia.cellerino@icloud.com>
2024-08-14 17:43:22 +0200

    Added Chrome Logo, centered heading and subheading

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import homeHeroImg from "../../assets/home-hero-image.png";
+import chrome from "../../assets/chrome.png";
 import { Button } from "../Button";
 import { Container } from "../Container";
 
 export const Hero = () => {
   return (
     <Container>
-      <div className="flex flex-row px-6 mt-2 items-start lg:items-center justify-center lg:justify-between">
-        <div className="flex flex-col gap-6 w-textContainer">
+      <div className="flex flex-row px-6 mt-2 items-start lg:items-center justify-center">
+        <div className="flex flex-col gap-6 w-textContainer3 text-center">
           <div className="font-sans text-5xl lg:text-7xl font-bold text-dark-purple leading-textHero2 lg:leading-textHero tracking-wider">
             Turbocharge your job search with{" "}
             <span className="text-light-purple">GigsHub</span>
           </div>
+
           <div className="flex justify-center">
             <img
               className="lg:hidden"
@@ -19,21 +21,20 @@ export const Hero = () => {
               alt="Women laughing in front of computers"
             />
           </div>
-          <p className="font-sans text-lg font-normal leading-7 text-dark-purple">
-          Discover the future of job search with Gigshub – 
-          automate your applications and focus on what truly matters - nailing interviews!
-
-          </p>
-          <div className="flex pt-4">
-            <Button>Get Hired</Button>
+          <div className="flex flex-col items-center">
+            <p className="font-sans text-lg font-normal leading-7 text-dark-purple">
+              We automate applications, tailor CVs, and ensure ideal matches, so
+              you can focus on what truly matters - nailing interviews.
+            </p>
+            <div className="flex pt-12">
+              <Button>
+                <div className="flex items-center gap-2">
+                  <img src={chrome} alt="Chrome logo" className="w-5 h-5" />
+                  <span>Add to Chrome</span>
+                </div>
+              </Button>
+            </div>
           </div>
-        </div>
-        <div>
-          <img
-            className="hidden lg:flex"
-            src={homeHeroImg}
-            alt="Women laughing in front of computers"
-          />
         </div>
       </div>
     </Container>
